5ceb4e6be34bebca0b0e76e87afde582d82463c8
Elivelton Costa <elivelton_costa@live.com>
2018-09-02 09:45:21 -0300

    Servidor com aplicação em Thread
    Utilização do _thread no Python 3

--- servidor.py
+++ servidor.py
@@ -2,6 +2,7 @@
 
 import socket
 import os
+import _thread
 
 os.system('clear')
 
@@ -10,33 +11,39 @@ PORT = 50000
 
 mensagens = []
 
+def conection(con, cliente):
+	print("Conectado com: " + str(cliente))
+
+	while True:
+		msg = con.recvfrom(1024)
+
+		if msg[0].decode('utf-8') == 'EXIT':
+			data = "' , '".join(mensagens)
+			data = data.encode('utf-8')
+			con.send(data)
+			print("Ultimas mensagens recebidas pelo servidor:\n" + str(mensagens))
+			con.close()
+			break
+		else:
+			mensagens.append(msg[0].decode('utf-8'))
+			if len(mensagens) == 6:
+				del mensagens[0]
+			print("Host " + cliente[0] +": ", msg[0].decode('utf-8'))
+
+	print('Finalizado conexão do cliente ', cliente[0])
+	_thread.exit()
+
+
+
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 tcp_socket.bind((HOST, PORT))
 tcp_socket.listen(1)
 
 print('Aguardando conexão')
-con, cliente = tcp_socket.accept()
-print("Conectado com: " + str(cliente[0]))
 	
 while True:
-	msg = con.recvfrom(1024)
+	con, cliente = tcp_socket.accept()
+	_thread.start_new_thread(conection, tuple([con, cliente]))
 
-	if msg[0].decode('utf-8') == 'EXIT':
-		data = "' , '".join(mensagens)
-		data = data.encode('utf-8')
-		con.send(data)
-		print("Ultimas mensagens recebidas pelo servidor:\n" + str(mensagens))
-		con.close()
-		break
-	else:
-		mensagens.append(msg[0].decode('utf-8'))
-		if len(mensagens) == 6:
-			del mensagens[0]
-		print("Host " + cliente[0] +": ", msg[0].decode('utf-8'))
-	
-	#txt = input('Digite a mensagem: ') 
-	#txt = txt.encode('utf-8')
-	#con.send(txt)
-  
-print('Finalizado conexão do cliente ', cliente[0])
+tcp.close()
