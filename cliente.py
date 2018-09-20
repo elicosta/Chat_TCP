2b1a03e192c254161767e760ce67dc359e3b4c0c
Elivelton Costa <elivelton_costa@live.com>
2018-09-20 17:23:54 -0300

    Alterado metodo _thread para threading no Python 3

--- cliente.py
+++ cliente.py
@@ -1,38 +1,60 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import socket
 import os
+import threading
+import time
 
 os.system('clear')
 
-HOST = '10.20.2.23'
-PORT = 50000
-NOME = None
+def escutar(con):
+	while True:
+		recvmsg = con.recv(1024)
+		recvmsg = recvmsg.decode('utf-8')
+		print("<Servidor>>> \n" + recvmsg)
 
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#------------------------------------------------------------------------------------------
+#Bloco Principal
+#------------------------------------------------------------------------------------------
+host = input('HOST: ')
+port = int(input('PORT: '))
 
-tcp_socket.connect((HOST, PORT))
+if (len(host) > 0):
+	host = host
+else:
+	host = 'localhost'
 
-NOME = input('Digite nome de usuário: ')
+if (port > 0 and port < 65535):
+	port = int(port)
+else:
+	print("porta inválida!!!\nUsando porta padrão: 50000")
+	port = 50000
 
-print("Conectado com: " + str(NOME))
-print('Para sair use EXIT\n')
+tcp_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_connect.connect((host, port))
 
-NOME = NOME.encode('utf-8')
-tcp_socket.send(NOME)
+ouvir = threading.Thread(target=escutar, args=(tcp_connect,))
+ouvir.setDaemon(True)
 
-msg = None
+
+nick = input('Digite nome de usuário: ')
+tcp_connect.send(nick.encode('utf-8'))
+
+print("Conectado como: " + str(nick))
+print('Para sair use CTRL+X\n')
+
+ouvir.start()
 
 while True:
-	
+
 	msg = input('Digite a mensagem: ')
 	msg = msg.encode('utf-8')
-	tcp_socket.send(msg)
+	tcp_connect.send(msg)
 
-	if msg.decode('utf-8') == 'EXIT':
-		BD = tcp_socket.recvfrom(1024)
-		BD = BD[0].decode('utf-8')
-		tcp_socket.close()
-		print("As mensagens guardadas no servidor: \n" + BD)
-		print("Conexão finalizada...")
+	if msg.decode('utf-8') == '\x18':
+		time.sleep(1)
 		break
+		
+tcp_connect.close()
+print("\nConexão finalizada...")
--- servidor.py
+++ servidor.py
@@ -1,53 +1,66 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 
 import socket
 import os
-import _thread
+import threading
+import time
 
-os.system('clear')
-
-HOST = ''
+HOST = 'localhost'
 PORT = 50000
 
 mensagens = []
 
+#def enviar(con, username, msg):
+#	broad = str(username + ": " + msg)
+#	broad = broad.encode('utf-8')
+#	con.send(broad)
+
 def conection(con, cliente):
-	username = con.recvfrom(1024)
-	username = username[0].decode('utf-8')
+	username = con.recv(1024)
+	username = username.decode('utf-8')
 
 	print(username + " conectou com o servidor...")
 
 	while True:
-		msg = con.recvfrom(1024)
-		msg = msg[0].decode('utf-8')
+		msg = con.recv(1024)
+		msg = msg.decode('utf-8')
 
-		if msg == 'EXIT':
+		if msg == '\x18':
 			data = "\n".join(mensagens)
 			data = data.encode('utf-8')
 			con.send(data)
-			#print("Ultimas mensagens recebidas pelo servidor:\n" + str(mensagens))
-			con.close()
+			time.sleep(1)
 			break
 		else:
 			mensagens.append(username + ": " + msg)
-			if len(mensagens) == 6:
-				del mensagens[0]
-			print(username + ": ", msg)
+			print(username + ": " + msg)
 
 	print(username + " desconectou do servidor...")
-	_thread.exit()
+	con.close()
 
-
+#------------------------------------------------------------
+# Bloco principal
+#------------------------------------------------------------
+os.system('clear')
 
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 tcp_socket.bind((HOST, PORT))
 tcp_socket.listen(1)
 
-print('Aguardando conexão')
+print('Servidor TCP iniciado no IP', HOST, 'na porta', PORT)
+print('\nAguardando conexão')
 	
 while True:
+	#Aceitando conexão
 	con, cliente = tcp_socket.accept()
-	_thread.start_new_thread(conection, tuple([con, cliente]))
 
-tcp.close()
+	# Thread de conexão
+	connect = threading.Thread(target=conection, args=(con, cliente))
+
+	#Iniciando a Thread e ativando daemon
+	connect.setDaemon(True)
+	connect.start()
+
+tcp_socket.close()
