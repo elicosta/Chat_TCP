9f59ad6f1d88d1a379ba01b0555dbe7554eff2f1
Elivelton Costa <elivelton_costa@live.com>
2018-09-23 13:22:58 -0300

    Adicionado autenticação pelo SUAP
    Utilização da API do SUAP

--- cliente.py
+++ cliente.py
@@ -5,30 +5,71 @@ import socket
 import os
 import threading
 import time
+import json
+import requests
+import getpass
 
 os.system('clear')
+
+#------------------------------------------------------------------------------------------
+#Recebendo mensagens do servidor TCP
+#------------------------------------------------------------------------------------------
 
 def escutar(con):
 	while True:
 		recvmsg = con.recv(1024)
 		recvmsg = recvmsg.decode('utf-8')
-		print("<Servidor>>> \n" + recvmsg)
+		print(recvmsg)
 
+#------------------------------------------------------------------------------------------
+#Recebendo dados do SUAP
+#------------------------------------------------------------------------------------------
+#Login para autenticar ao Chat TCP
+matricula = input("Informe sua matrícula: ")
+senha = getpass.getpass("Infome sua senha: ")
+#senha = input("Infome sua senha: ")
+
+autenticacao = {
+    "username": str(matricula),
+    "password": str(senha)
+}
+
+urls = { "token":"https://suap.ifrn.edu.br/api/v2/autenticacao/token/",
+         "dados":"https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/"}
+
+def getToken():
+    response = requests.post(urls['token'], data=autenticacao)
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))['token']
+    return None
+
+cabecalho={'Authorization': 'JWT {0}'.format(getToken())}
+
+def getInformacoes():
+    response = requests.get(urls['dados'], headers=cabecalho)
+    if response.status_code == 200:
+        return response.content.decode('utf-8')
+    return None
+
+informacoes = json.loads(getInformacoes())
 #------------------------------------------------------------------------------------------
 #Bloco Principal
 #------------------------------------------------------------------------------------------
+os.system('clear')
+
 host = input('HOST: ')
 port = int(input('PORT: '))
+
+os.system('clear')
 
 if (len(host) > 0):
 	host = host
 else:
 	host = 'localhost'
 
-if (port > 0 and port < 65535):
+if (len(host) > 0):
 	port = int(port)
 else:
-	print("porta inválida!!!\nUsando porta padrão: 50000")
 	port = 50000
 
 tcp_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,23 +78,24 @@ tcp_connect.connect((host, port))
 ouvir = threading.Thread(target=escutar, args=(tcp_connect,))
 ouvir.setDaemon(True)
 
+tcp_connect.send(informacoes['nome_usual'].encode('utf-8'))
 
-nick = input('Digite nome de usuário: ')
-tcp_connect.send(nick.encode('utf-8'))
-
-print("Conectado como: " + str(nick))
+print("Conectado como: " + informacoes['nome_usual'])
 print('Para sair use CTRL+X\n')
 
 ouvir.start()
 
+time.sleep(2)
 while True:
-
-	msg = input('Digite a mensagem: ')
+	time.sleep(0.5)
+	msg = input()
 	msg = msg.encode('utf-8')
 	tcp_connect.send(msg)
 
 	if msg.decode('utf-8') == '\x18':
-		time.sleep(1)
+		#BD = tcp_connect.recv(1024)
+		#BD = BD.decode('utf-8')
+		#print("As mensagens guardadas no servidor: \n" + BD)
 		break
 		
 tcp_connect.close()
