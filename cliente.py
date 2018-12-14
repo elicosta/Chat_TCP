10e5b68eacdb716d472250bcc3fb0e0c64eef96d
Elivelton Costa <elivelton_costa@live.com>
2018-12-13 22:00:57 -0300

    Implementado comunicação com PostgreSQL

--- cliente.py
+++ cliente.py
@@ -8,8 +8,11 @@ import time
 import json
 import requests
 import getpass
+import psycopg2
 
 os.system('clear')
+
+ConexaoDBmgs = "dbname=bdmgs user=postgres host=localhost password=aluno"
 
 #------------------------------------------------------------------------------------------
 #Recebendo mensagens do servidor TCP
@@ -55,11 +58,29 @@ def getInformacoes():
 informacoes = json.loads(getInformacoes())
 
 #------------------------------------------------------------------------------------------
+#Recebendo dados do BD
+#------------------------------------------------------------------------------------------
+def RecuperaBD():
+	connConexao = psycopg2.connect(ConexaoDBmgs)
+	curConexao = connConexao.cursor()
+	curConexao.execute("select * from bdmgs")
+	mensagens = curConexao.fetchall()
+	curConexao = connConexao.cursor()
+	connConexao.commit()
+	connConexao.close()
+	
+	print ("Ultimas mensagens do banco de dados:")
+	
+	for mensagem in mensagens:
+		print ("Em {0} às {1} no IP {2}\n\t- {3}: {4}\n".format(mensagem[1], mensagem[2], mensagem[4], mensagem[0], mensagem[3]))
+
+
+#------------------------------------------------------------------------------------------
 #Bloco Principal
 #------------------------------------------------------------------------------------------
 os.system('clear')
 
-print("Por padrão:\nHOST: localhost\nPORT: 50000")
+print("Por padrão: \n\tHOST: localhost PORT: 50000\n")
 
 host = input('HOST: ')
 port = input('PORT: ')
@@ -80,28 +101,33 @@ else:
 tcp_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_connect.connect((host, port))
 
+#Enviando informação da API do SUAP para o Servidor
+tcp_connect.send(informacoes['nome_usual'].encode('utf-8'))
+
 #Criando thread para o broadcast do Servidor
 ouvir = threading.Thread(target=escutar, args=(tcp_connect,))
 ouvir.setDaemon(True)
 
-#Enviando informação da API do SUAP para o Servidor
-tcp_connect.send(informacoes['nome_usual'].encode('utf-8'))
-
 print("Conectado como: " + informacoes['nome_usual'])
 print('Para sair use CTRL+X\n')
+
+#Recuperando dados do Banco de dados
+RecuperaBD()
 
 #Ativando broadcast do cliente
 ouvir.start()
 
-time.sleep(1)
+#time.sleep(1)
 
 #Estrutura de envio de mensagens para o servidor
 while True:
 	msg = input()
+
 	if msg == '\x18':
 		break
 	else:
 		tcp_connect.send(msg.encode('utf-8'))
+		time.sleep(1)
 	
 #Finalizando conexão
 tcp_connect.close()
--- servidor.py
+++ servidor.py
@@ -5,6 +5,7 @@ import os
 import time
 import socket
 import socketserver
+import psycopg2
 
 HOST = ''
 PORT = 50000
@@ -12,6 +13,64 @@ PORT = 50000
 clientes = []
 nomes = []
 mensagens = []
+
+###Comando de conexões ao Banco###
+ConexaoDefault  = "dbname=postgres user=postgres host=localhost password=aluno"
+ConexaoDBmgs = "dbname=bdmgs user=postgres host=localhost password=aluno"
+SQLCriaDatabase = "CREATE DATABASE bdmgs"
+
+###Criar Tabela###
+SQLCriaTable = "CREATE TABLE bdmgs (nome VARCHAR(100), data VARCHAR(20), hora VARCHAR(20), mensagem VARCHAR(200), endereco VARCHAR(30));"
+
+
+#------------------------------------------------------------------------------------------
+#Funções para o Banco de Dados
+#------------------------------------------------------------------------------------------
+def tabela_existe(NomeTabela):
+	strSQL1 = "SELECT EXISTS(SELECT datname FROM pg_database WHERE datname='{0}')".format(NomeTabela)
+	strSQL = "SELECT EXISTS(SELECT relname FROM pg_class WHERE relname='{0}')".format(NomeTabela)
+
+	conn = psycopg2.connect(ConexaoDefault)
+	curbd = conn.cursor()
+	curbd.execute(strSQL1)
+	existeBD = curbd.fetchone()[0]
+
+	if(existeBD == True):
+		con = psycopg2.connect(ConexaoDBmgs)
+		cur = con.cursor()
+		cur.execute(strSQL)
+		existe = cur.fetchone()[0]
+		cur.close()
+		con.close()
+	else:
+		existe = False
+
+	curbd.close()
+	conn.close()
+
+	return existe
+
+def criarBanco():
+	from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+	conn = psycopg2.connect(ConexaoDefault)
+	conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+	cur = conn.cursor()
+	cur.execute(SQLCriaDatabase)
+	conn = psycopg2.connect(ConexaoDBmgs)
+	cur = conn.cursor()
+	cur.execute(SQLCriaTable)
+	conn.commit()
+	cur.close()
+	conn.close()
+
+def Guardarmsg(nome, data, hora, mensagem, endereco):
+	conn = psycopg2.connect(ConexaoDBmgs)
+	cur = conn.cursor()
+	SQLInsereDados = "INSERT INTO bdmgs (nome, data, hora, mensagem, endereco) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')".format(nome, data, hora, mensagem, endereco)
+	cur.execute(SQLInsereDados)
+	conn.commit()
+	cur.close()
+	conn.close()
 
 #------------------------------------------------------------------------------------------
 #Funções do servidor
@@ -29,20 +88,22 @@ class MyTCPServer(socketserver.BaseRequestHandler):
 
 		print(str(len(clientes)) + " usuários online")
 
-		#Envio das mensagens que estão armazenadas no servidor
-		BD = "\n".join(mensagens)
-		self.request.send(BD.encode('utf-8'))
-
 		time.sleep(1)
 
 		while True:
 			try:
-				data = self.request.recv(1024) #Recebendo informações dos clientes
-				data = data.decode('utf-8')
-				print (self.nickname + "(" + self.client_address[0] + "): " + data)
-				out = str("- " + self.nickname + ": " + data)
-				mensagens.append(out) #Armazenando informações no servidor
+				mensagem = self.request.recv(1024) #Recebendo informações dos clientes
+				mensagem = mensagem.decode('utf-8')
+				hora = time.strftime('%H:%M:%S')
+				data = time.strftime('%d-%m-%Y')
+
+				print (self.nickname + "(" + self.client_address[0] + "): " + mensagem)
+				out = str("- " + self.nickname + ": " + mensagem)
 				self.broadcast(out) #Envio das mensagens para seus clientes
+
+				if(mensagem !=''):
+					Guardarmsg(self.nickname, data, hora, mensagem, self.client_address[0])
+				
 		        
 			except:
 				#Quando é finalizado conexão do cliente para o Servidor
@@ -53,11 +114,11 @@ class MyTCPServer(socketserver.BaseRequestHandler):
 				self.request.close()
 				print(str(len(clientes)) + " usuários online\n")
 
-				return
+				return			
         
-	def broadcast(self, data):
+	def broadcast(self, mensagem):
 		for user in clientes:
-			user.send(data.encode('utf-8'))
+			user.send(mensagem.encode('utf-8'))
 
 	def disconnection(self):
 		return "\n" + self.nickname + "(" + self.client_address[0] + ") Desconectado de " + time.strftime('%H:%M') + "\n"
@@ -72,7 +133,7 @@ class MyTCPServer(socketserver.BaseRequestHandler):
 		nomes.append(self.nickname)
 
 class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-		pass
+	pass
 
 #Criando Servidor com base no IP e porta 50000
 server = ThreadingTCPServer((HOST, PORT), MyTCPServer)
@@ -83,10 +144,15 @@ server = ThreadingTCPServer((HOST, PORT), MyTCPServer)
 os.system('clear')
 
 print("Bem-vindo ao CHAT\n")
-print("Hora local: " + time.strftime('%H:%M') + " | " + str(len(clientes)) + " usuário(s)")  
+print("Hora local: " + time.strftime('%H:%M') + " | " + str(len(clientes)) + " usuário(s)")
+
+
 print('Servidor iniciado no IP na porta', PORT)
 print('Para encerrar use CTRL+C\n')
 print('\nAguardando conexões')
+
+if (tabela_existe("bdmgs") == False):
+	criarBanco()
 
 #Iniciando servidor socket
 server.serve_forever()
